6819b40f4de1b0bf596f2d9c3844060dcb92c3ba
humayonitbd <humayonforid44@gmail.com>
2024-03-14 16:10:22 +0600

    modal add

---
+++ src/components/tasks/AddTaskModal.jsx
@@ -0,0 +1,12 @@
+import Modal from "../ui/Modal";
+
+
+const AddTaskModal = ({isOpen,setIsOpen}) => {
+    return (
+      <Modal isOpen={isOpen} setIsOpen={setIsOpen} title="Programming Hero !">
+        <p>Lorem ipsum dolor sit fffafa.</p>
+      </Modal>
+    );
+};
+
+export default AddTaskModal;
